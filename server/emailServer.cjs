e332a9e2c25d5bf9688711806b277d912bbd7657
Chathura Sudaraka <Chathurabro68@gmail.com>
2025-07-07 22:14:05 +0530

    Update

--- server/emailServer.cjs
+++ server/emailServer.cjs
@@ -26,7 +26,6 @@ app.use(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "http://localhost:8080",
-        "https://chathura.eversoft.lk",
       ];
 
       if (allowedOrigins.includes(origin) || !origin) {
